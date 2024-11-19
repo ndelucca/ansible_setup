566ee1d02b0ce6bbd19d21392feee4f68af6e9fa
ndelucca <nazadelucca@gmail.com>
2024-11-18 21:02:14 -0300

    refactor app installation system

---
+++ app_roles/terminal/files/install_wezterm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+
+apt update
+
+apt install -y wezterm
